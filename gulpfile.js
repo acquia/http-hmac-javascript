28ced153e70001eaf0747f444926f7e559adaad0
Yuan Xie <yuan.xie@acquia.com>
2016-01-31 20:48:56 -0500

    LEX-88 Use es6 in gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var del = require('del');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const del = require('del');
 
-gulp.task('clean-demo', function(){
-  return del(['./demo']);
+gulp.task('clean-demo', () => {
+  del(['./demo']);
 });
 
-gulp.task('clean-lib', function(){
-  return del(['./lib']);
+gulp.task('clean-lib', () => {
+  del(['./lib']);
 });
 
-gulp.task('build-lib',['clean-lib'], function(){
+gulp.task('build-lib',['clean-lib'], () => {
   gulp.src(['./src/hmac.js'])
     .pipe(concat('hmac.js'))
     .pipe(gulp.dest('./lib/es6'))
@@ -26,10 +26,10 @@ gulp.task('build-lib',['clean-lib'], function(){
     .pipe(gulp.dest('./lib/es5'));
 });
 
-gulp.task('build-demo',['clean-demo', 'build-lib'], function(){
-  gulp.src('./src/demo/*.html', {base: './src/demo'})
+gulp.task('build-demo',['clean-demo', 'build-lib'], () => {
+  gulp.src('./src/demo/*.html')
     .pipe(gulp.dest('./demo'));
-  gulp.src('./src/demo/*.php', {base: './src/demo'})
+  gulp.src('./src/demo/*.php')
     .pipe(gulp.dest('./demo'));
   gulp.src(['./src/hmac.js', './src/demo/get.js'])
     .pipe(concat('get.app.js'))
@@ -45,11 +45,11 @@ gulp.task('build-demo',['clean-demo', 'build-lib'], function(){
     .pipe(gulp.dest('./demo'));
 });
 
-gulp.task('default', ['build-demo'], function() {
-  gulp.watch('src/*.js', function() {
+gulp.task('default', ['build-demo'], () => {
+  gulp.watch('src/*.js', () => {
     gulp.run('build-demo');
   });
-  gulp.watch('src/demo/*', function() {
+  gulp.watch('src/demo/*', () => {
     gulp.run('build-demo');
   });
 });
